6691dc7f962474534f3ce4d50529f69009381e93
YOUSSEF-BOUJYDAH <youssef.boujydah@ump.ac.ma>
2022-11-12 22:54:53 +0100

    final setup

--- backend/settings.py
+++ backend/settings.py
@@ -84,9 +84,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
+   # "http://127.0.0.1:4200",
 ]
 
 # Password validation
